959ca83b08b68f33c2c3421b2782e08922bf9309
Cullen <cullenfitzgerald9@gmail.com>
2022-01-26 09:09:18 -0500

    commit

--- main.py
+++ main.py
@@ -6,24 +6,25 @@ import numpy as np
 
 
 # Main function
-def kmeans(k):
+def kmeans(k, blk):
     # image to be used
-    image = cv2.imread(r"C:\Users\Cullen\Pictures\Camera Roll\PicOfMe.PNG")
+    if blk:
+        image = cv2.imread(r"C:\Users\Cullen\Pictures\Camera Roll\PicOfMe.PNG")
+    else:
+        image = cv2.imread(r"C:\Users\Cullen\Pictures\normalPic.png")
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
     # reshape image matrix
     image = image.reshape((image.shape[1] * image.shape[0], 3))
 
     finArr = []
-    for i in range(10):
+    for i in range(20):
         kmeans = KMeans(n_clusters=k)
         s = kmeans.fit(image)
 
         # These are the values that are assigned to each pixel. Labels is the array that we want to average
         labels = kmeans.labels_
-        #labels = list(labels)
         finArr.append(labels)
-        #print(finArr[i])
 
     plots = []
     for i in range(k):
@@ -35,12 +36,11 @@ def kmeans(k):
             tot = temp.count(i)
             tot = tot / 10
             finMat.append(tot)
-        #print(finMat)
         finMat = np.reshape(finMat, (-1, 2))
         heat_map = sns.heatmap(finMat,cmap='gist_ncar')
         heat_map.set_title("Cluster " + str(i))
-        #heat_map = plt.subplots(i)
-        #plt.ion()
+        plt.xticks([])
+        plt.yticks([])
         plt.show()
 
-kmeans(3)
+kmeans(3, blk=False)
